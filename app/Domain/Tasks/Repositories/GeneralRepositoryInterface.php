a533b633f8f7e152f393dd5df41a17c186395103
IVeryHateMonday <kovalinskijoleksandr38@gmail.com>
2026-04-06 14:16:19 +0000

    Add GeneralRepositoryInterface

--- app/Domain/Tasks/Repositories/GeneralRepositoryInterface.php
+++ app/Domain/Tasks/Repositories/GeneralRepositoryInterface.php
@@ -5,4 +5,7 @@ namespace App\Domain\Tasks\Repositories;
 interface GeneralRepositoryInterface
 {
     public function save(array $data): void;
+    public function getById(int $id);
+    public function delete(int $id): void;
+
 }
